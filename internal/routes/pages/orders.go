f42a3c5ad5a19263d1930bcfd10a21b2e00f3618
Marco Antônio <ma.souza.junior@gmail.com>
2023-12-07 15:10:31 -0300

    fix: order page props

--- internal/routes/pages/orders.go
+++ internal/routes/pages/orders.go
@@ -3,18 +3,27 @@ package pages
 import (
 	"github.com/gofiber/fiber/v2"
 
+	"github.com/marco-souza/marco.fly.dev/internal/config"
 	"github.com/marco-souza/marco.fly.dev/internal/models"
 )
+
+type ordersProps struct {
+	config.PageParams
+	Title  string
+	Total  int64
+	Orders []models.Order
+}
 
 func ordersHandler(c *fiber.Ctx) error {
 	db := models.Connect()
 	orders := []models.Order{}
 	result := db.Preload("Author").Find(&orders)
 
-	props := fiber.Map{
-		"Title":  "All Orders",
-		"Orders": orders,
-		"Total":  result.RowsAffected,
+	props := ordersProps{
+		PageParams: config.DefaultPageParams,
+		Title:      "All Orders",
+		Orders:     orders,
+		Total:      result.RowsAffected,
 	}
 
 	return c.Render("orders", props)
