2375c09a0dfb2d923a1c13f4811f75c4106b6816
Marco Antônio <ma.souza.junior@gmail.com>
2023-12-07 14:58:10 -0300

    enh: load orders page on the server

--- internal/routes/api/orders.go
+++ internal/routes/api/orders.go
@@ -10,8 +10,8 @@ func ordersHandler(c *fiber.Ctx) error {
 	db := models.Connect()
 	orders := []models.Order{}
 	result := db.Preload("Author").Find(&orders)
-
 	props := fiber.Map{"Orders": orders, "Total": result.RowsAffected}
+
 	return c.Render("partials/order-list", props, "layouts/empty")
 }
 
--- internal/routes/pages/orders.go
+++ internal/routes/pages/orders.go
@@ -2,12 +2,21 @@ package pages
 
 import (
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/marco-souza/marco.fly.dev/internal/models"
 )
 
 func ordersHandler(c *fiber.Ctx) error {
+	db := models.Connect()
+	orders := []models.Order{}
+	result := db.Preload("Author").Find(&orders)
+
 	props := fiber.Map{
-		"Title": "All Orders",
+		"Title":  "All Orders",
+		"Orders": orders,
+		"Total":  result.RowsAffected,
 	}
+
 	return c.Render("orders", props)
 }
 
