34dec091570df9142a3443b6fdf9706330c487d4
Marco Antônio <ma.souza.junior@gmail.com>
2024-05-09 17:12:37 -0300

    enh: make hot-reload faster

--- internal/routes/api/sse.go
+++ internal/routes/api/sse.go
@@ -69,7 +69,7 @@ func sseReloadHandler(c *fiber.Ctx) error {
 				fmt.Printf("Error while flushing: %v. Closing http connection.\n", err)
 				break
 			}
-			time.Sleep(time.Millisecond * 100)
+			time.Sleep(time.Millisecond * 10)
 		}
 	}))
 
