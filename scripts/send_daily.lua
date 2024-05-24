c0878b7dc3600fe8d33de767e73b36d5ed2670a2
Marco Antônio <ma.souza.junior@gmail.com>
2024-05-24 13:49:43 -0300

    oth: update lua script

--- scripts/send_daily.lua
+++ scripts/send_daily.lua
@@ -1,16 +1,22 @@
 local channel_map = {
     daily = "742025222126960760",
+    geral = "694542727358185502",
     testing = "1243281325780107444",
+}
+
+local user_map = {
+    marco = "488746421944582154",
+    nicolas = "694959368827043880",
+    vitor = "311246917248221184",
+    rafael = "139083790344650753",
 }
 
 if not discord.is_work_day() then
     return
 end
 
-message_sent = discord.send_message(
-    channel_map.daily, "Hello, World!"
-)
-if message_sent then
+local message = string.format("Hello, <@%s>! Bora acordar cachorro 🐶", user_map.marco)
+if discord.send_message(channel_map.testing, message) then
     -- check if it's not a weekend
     print("message sent! 🎉")
 end
