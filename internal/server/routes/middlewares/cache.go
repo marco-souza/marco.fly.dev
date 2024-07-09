0b4ee4e2a4cd65ef2735bd09c51de4b725cecdd5
Marco Antônio <ma.souza.junior@gmail.com>
2024-07-09 08:53:31 -0300

    enh: improve logs

--- internal/server/routes/middlewares/cache.go
+++ internal/server/routes/middlewares/cache.go
@@ -2,17 +2,31 @@ package middlewares
 
 import (
 	"log"
+	"slices"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cache"
 )
 
+var logger = log.New(log.Writer(), "cache: ", log.Flags())
+
 var Cache = cache.New(cache.Config{
 	Next: func(c *fiber.Ctx) bool {
-		isCacheDisabled := c.Query("noCache") == "true"
-		log.Println("Is cache disabled: ", isCacheDisabled)
-		return isCacheDisabled
+		if cacheControl, ok := c.GetReqHeaders()["Cache-Control"]; !ok {
+			if slices.Contains(cacheControl, "no-cache") {
+				logger.Println("disabled by cache control", cacheControl)
+				return false
+			}
+		}
+
+		if c.Query("noCache") == "true" {
+			logger.Println("disabled by query param")
+			return false
+		}
+
+		// skip middleware if cache enable
+		return true
 	},
 	Expiration:   15 * time.Minute,
 	CacheControl: true,
