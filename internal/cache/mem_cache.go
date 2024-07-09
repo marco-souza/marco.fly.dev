8827ba4f355497619a1daba001461bd2f94d81e7
Marco Antônio <ma.souza.junior@gmail.com>
2024-07-09 11:42:31 -0300

    fix: make storage private for mem cache

--- internal/cache/mem_cache.go
+++ internal/cache/mem_cache.go
@@ -6,17 +6,17 @@ import (
 )
 
 type MemCache struct {
-	Storage map[string][]byte
+	storage map[string][]byte
 }
 
 func NewMemCache() MemCache {
 	return MemCache{
-		Storage: make(map[string][]byte),
+		storage: make(map[string][]byte),
 	}
 }
 
 func (c MemCache) Get(key string) ([]byte, error) {
-	value, ok := c.Storage[key]
+	value, ok := c.storage[key]
 	if !ok {
 		return nil, fmt.Errorf("cache miss for key: %s", key)
 	}
@@ -28,19 +28,19 @@ func (c MemCache) Set(key string, value []byte, opts *CacheOptions) error {
 	if opts != nil && opts.ttl != 0 {
 		go (func() {
 			time.Sleep(opts.ttl)
-			delete(c.Storage, key)
+			delete(c.storage, key)
 			logger.Printf("key `%s` has been deleted\n", key)
 		})()
 	}
 
-	c.Storage[key] = value
+	c.storage[key] = value
 	logger.Printf("cacheed key `%s`\n", key)
 	return nil
 }
 
 func (c MemCache) Flush() error {
-	for key := range c.Storage {
-		delete(c.Storage, key)
+	for key := range c.storage {
+		delete(c.storage, key)
 	}
 
 	return nil
