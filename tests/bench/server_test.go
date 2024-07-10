5e7cc2084f1108c92331c55cb6ba70f5864fd1cb
Marco Antônio <ma.souza.junior@gmail.com>
2024-07-10 17:24:09 -0300

    enh: split benchmarks

--- tests/bench/server_test.go
+++ tests/bench/server_test.go
@@ -9,9 +9,30 @@ import (
 	"github.com/marco-souza/marco.fly.dev/internal/server"
 )
 
-var baseUrl = "https://marco.fly.dev"
+func BenchmarkProdServer(b *testing.B) {
+	baseUrl := "https://marco.fly.dev"
 
-func BenchmarkServer(b *testing.B) {
+	b.ResetTimer() // Reset the benchmark timer
+
+	b.Run("sync GET /", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			http.Get(baseUrl + "/")
+		}
+	})
+
+	b.Run("async GET /", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				http.Get(baseUrl + "/")
+			}
+		})
+	})
+}
+
+func BenchmarkLocalServer(b *testing.B) {
+	baseUrl := "http://localhost:3001"
+
+	// set view folder
 	b.Setenv("VIEWS", "../../views/")
 
 	// disabling logs
@@ -26,29 +47,13 @@ func BenchmarkServer(b *testing.B) {
 
 	b.ResetTimer() // Reset the benchmark timer
 
-	b.Run("prod sync GET /", func(b *testing.B) {
+	b.Run("sync GET /", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			http.Get(baseUrl + "/")
 		}
 	})
 
-	b.Run("prod async GET /", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				http.Get(baseUrl + "/")
-			}
-		})
-	})
-
-	baseUrl = "http://localhost:3001"
-
-	b.Run("local sync GET /", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			http.Get(baseUrl + "/")
-		}
-	})
-
-	b.Run("local async GET /", func(b *testing.B) {
+	b.Run("async GET /", func(b *testing.B) {
 		b.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
 				http.Get(baseUrl + "/")
