8f95bf704844f5fc36f10732fa87cf950c5c5f7c
Marco Antônio <ma.souza.junior@gmail.com>
2024-07-10 11:52:34 -0300

    fea: add integration tests for server

--- tests/server_test.go
+++ tests/server_test.go
@@ -2,25 +2,60 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"io"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/marco-souza/marco.fly.dev/internal/server"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCanCreateServer(t *testing.T) {
+	t.Setenv("VIEWS", "../views/")
+
 	s := server.New()
 	values := fmt.Sprintf("%v", s)
+	baseUrl := "http://localhost:3001"
 
 	t.Run("validate env configs", func(t *testing.T) {
-		if s == nil {
-			t.Fatal("Failed to create server")
-		}
-		if !strings.Contains(values, "localhost") {
-			t.Fatal("No host found")
-		}
-		if !strings.Contains(values, "3001") {
-			t.Fatal("No port found")
-		}
+		assert.NotNil(t, s)
+		assert.Contains(t, values, "localhost")
+		assert.Contains(t, values, "3001")
+	})
+
+	t.Run("can start server", func(t *testing.T) {
+		done := make(chan bool)
+		go s.Start(&done)
+
+		assert.True(t, <-done)
+		fmt.Println("server started")
+	})
+
+	// t.Run("can shutdown server", func(t *testing.T) {
+	// 	s.Shutdown()
+	// 	fmt.Println("server is down")
+	// })
+
+	t.Run("can fetch home /", func(t *testing.T) {
+		req := httptest.NewRequest("GET", baseUrl+"/", nil)
+
+		fmt.Printf("req: %v\n", req)
+
+		// http.Response
+		resp, err := s.Test(req, 5000)
+		assert.NoError(t, err)
+		assert.Equal(t, resp.StatusCode, 200)
+
+		body, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
+		assert.Contains(t, string(body), "Marco")
+	})
+
+	t.Run("can shutdown server", func(t *testing.T) {
+		s.Shutdown()
+		fmt.Println("server stopped")
 	})
 }
+
+// req := httptest.NewRequest("GET", "http://google.com", nil)
+// req.Header.Set("X-Custom-Header", "hi")
