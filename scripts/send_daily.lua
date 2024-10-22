d3e44a902318a057a7e2d5ce88285ba5b95ab6d2
Marco Antônio <ma.souza.junior@gmail.com>
2024-10-22 09:15:40 -0300

    enh: send_daily: add more daily messages

--- scripts/send_daily.lua
+++ scripts/send_daily.lua
@@ -1,4 +1,9 @@
 --cron: 20 4,16 * * *
+if not discord.is_work_day() then
+    print("it's weekend! 🏖️🍻")
+    -- discord.send_message(channel_map.geral, "É fim de semana galera, lembrem de descansar e aproveitar! 🏖️🍻")
+    return
+end
 
 local channel_map = {
     daily = "742025222126960760",
@@ -14,17 +19,22 @@ local user_map = {
 }
 
 local messages = {
-    daily = "Bom dia @here! Bora compartilhar sua Daily? 🐶🤖",
-    alert_marco = string.format("Hello, <@%s>! Me respeita soooooo celoooko??", user_map.marco)
+    dailys = {
+        "Bom dia @here! Bora compartilhar sua Daily? 🐶🤖",
+        "Alou @here! Quem vai compartilhar sua Daily hoje? 🐶🤖",
+        "Galeralera @here! Bora compartilhar sua Daily? 🐶🤖",
+        "@here Aobo bão? Quem vai participar da Daily hoje? 🐶🤖",
+        "Pulei no muro e cai de costa, @here quem vai postar a daily hoje seus bosta? 🐶🤖",
+        string.format(
+            "@here quem não postar a daily hoje vai ter %d comentários no seu próximo code review 🐶🤖",
+            math.random(13, 42)
+        ),
+    },
+    alert_marco = string.format("Hello, <@%s>! Me respeita soooooo celoooko??", user_map.marco),
 }
 
-if not discord.is_work_day() then
-    print("it's weekend! 🏖️🍻")
-    discord.send_message(channel_map.geral, "É fim de semana galera, lembrem de descansar e aproveitar! 🏖️🍻")
-    return
-end
-
-if discord.send_message(channel_map.daily, messages.daily) then
+local msg = messages.dailys[math.random(#messages.dailys)]
+if discord.send_message(channel_map.daily, msg) then
     -- check if it's not a weekend
     print("message sent! 🎉")
 end
